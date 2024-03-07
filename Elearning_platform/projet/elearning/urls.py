2948369eeda94faa4754ce9e2640cc8072de373e
islamouahab <islamouahab14@gmail.com>
2024-03-07 01:09:45 +0100

    adding authentification system

---
+++ Elearning_platform/projet/elearning/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('', views.login_view , name = 'login'),
+    path('login',views.login_handle , name="login_handle"),
+    path('logout',views.logout_handle, name="logout")
+]

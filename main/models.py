45bad68f7363a038e839f57720a8e3fd0c8e5c46
islamouahab <islamouahab14@gmail.com>
2024-04-01 19:09:38 +0100

    profile page done

--- main/models.py
+++ main/models.py
@@ -1,2 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class custom_user(AbstractUser):
+    phone_num = models.CharField(max_length=15)
+    profile_pic = models.ImageField(upload_to='profile_pics/')
+
+    def __str__(self):
+        return self.username
--- main/url.py
+++ main/url.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('login/authenticate', views.login_handle , name='login_handle'),
     path('logout',views.logout_handle, name="logout"),
     path('create_acc',views.create_user, name='create_user'),
-    path('profile/<int:user_id>',views.profile , name='profile')
+    path('profile/<int:user_id>',views.profile , name='profile'),
+    path('profile/<int:user_id>/update_profile',views.update_profile , name = 'update_profile'),
+    path('profile/<int:user_id>/delete',views.delete_user , name='delete_user')
 ]

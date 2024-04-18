736f957881513a0dbea3039dc96a7cd2ef6e39a9
islamouahab <islamouahab14@gmail.com>
2024-04-18 15:26:20 +0100

    created post and media files models

--- main/models.py
+++ main/models.py
@@ -7,3 +7,16 @@ class custom_user(AbstractUser):
 
     def __str__(self):
         return self.username
+class post(models.Model):
+    user_id = models.ForeignKey("custom_user",on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    space = models.FloatField()
+    floor_num = models.IntegerField()
+    description = models.CharField(max_length=1024)
+    price = models.FloatField()
+    def __str__(self):
+        return self.title
+    
+class media_files(models.Model):
+    post_id = models.ForeignKey("post",on_delete=models.CASCADE)
+    path = models.ImageField(upload_to='post_pics/')
